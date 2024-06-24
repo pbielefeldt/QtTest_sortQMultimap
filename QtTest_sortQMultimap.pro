1ea3d32af1ddb47e0b4b9aa33dd4ddd16b817040
Philipp Bielefeldt <bielefeldt@uni-bonn.de>
2024-06-24 12:55:40 +0200

    inital files

---
+++ QtTest_sortQMultimap.pro
@@ -0,0 +1,11 @@
+QT       += core
+QT       -= gui
+
+TARGET   = QtTest_sortQMultimap
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+# Input
+SOURCES  += main.cpp
---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+#include <QMultiMap>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QMultiMap<QString, QString> myMultiMap;
+    myMultiMap.insert("Key1", "Value1");
+    myMultiMap.insert("Key1", "Value2");
+    myMultiMap.insert("Key2", "Value3");
+
+    QStringList values = myMultiMap.values("Key1");
+    foreach (const QString &value, values) {
+        qDebug() << value;
+    }
+
+    return a.exec();
+}
