072d29a3649c23863743cac7c18a953449946c46
Philipp Bielefeldt <p.bielefeldt@secure.mailbox.org>
2024-06-24 14:28:08 +0000

    try to insert at upperBound(key)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <QCoreApplication>
+#include <QCoreApplication>
 #include <QDebug>
 #include <QMultiMap>
 
@@ -7,35 +7,35 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     QMultiMap<QString, QString> myMultiMap;
-    myMultiMap.insert("Key1", "f Value5");
-    myMultiMap.insert("Key1", "d Value4");
-    myMultiMap.insert("Key1", "b Value3");
-    myMultiMap.insert("Key1", "a Value2");
-    myMultiMap.insert("Key1", "c Value1");
-    myMultiMap.insert("Key1", "e Value7");
-	
+    myMultiMap.insert(myMultiMap.upperBound("Key1"), "Key1", "e Value5");
+    myMultiMap.insert(myMultiMap.upperBound("Key1"), "Key1", "c Value4");
+    myMultiMap.insert(myMultiMap.upperBound("Key1"), "Key1", "a Value3");
+    myMultiMap.insert(myMultiMap.upperBound("Key1"), "Key1", "b Value2");
+    myMultiMap.insert(myMultiMap.upperBound("Key1"), "Key1", "d Value1");
+
     myMultiMap.insert("Key2", "3 ValueA");
     myMultiMap.insert("Key2", "1 ValueB");
     myMultiMap.insert("Key2", "2 ValueC");
     myMultiMap.insert("Key2", "4 ValueD");
 
-    const QString key("Key1");
-
-    qDebug("Iterate in \"forward\" direction:\n");
-    QStringList values = myMultiMap.values("Key1");
-    foreach (const QString &value, values) {
+    QString key("Key1");
+    qDebug("Iterate in Key1 \"forward\" direction:\n");
+    foreach (const QString &value, myMultiMap.values(key)) {
         qDebug() << value;
     }
 
-    // this actually fixes it
-	qDebug("\n\nIterate in again, using \"--it\" direction:\n");
+    /*
+    // this actually "fixes" it
+    qDebug("\n\nIterate in again, using \"--it\" direction:\n");
     QMultiMap<QString, QString>::iterator rit = myMultiMap.upperBound(key);
-    QMultiMap<QString, QString>::iterator end = myMultiMap.lowerBound(key);
+    QMultiMap<QString, QString>::iterator end = myMultiMap.lowerBound(key)-1;
 
+    --rit;
     while (rit != end) {
-        --rit;
         qDebug() << rit.value();
+        --rit;
     }
+    */
 
     return a.exec();
 }
